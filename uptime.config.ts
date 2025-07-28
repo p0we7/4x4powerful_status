1d4bb5b725b3c8fa9e7e1bf3fd405a7d54e978b0
p0we7 <p0we7@users.noreply.github.com>
2025-07-28 13:33:38 +0800

    update target

--- uptime.config.ts
+++ uptime.config.ts
@@ -28,7 +28,7 @@ const workerConfig: WorkerConfig = {
       // `method` should be a valid HTTP Method
       method: 'GET',
       // `target` is a valid URL
-      target: 'https://4x4powerful.com',
+      target: 'https://www.4x4powerful.com',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       // tooltip: 'This is a tooltip for this monitor',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
